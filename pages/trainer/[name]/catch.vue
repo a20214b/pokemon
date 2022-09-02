a04256586a7786d82d892a53bfce2c33cfafcccf
ayumi.ito@konicaminolta.com <ayumi.ito@konicaminolta.com>
2022-09-02 18:33:06 +0900

    fix commit miss

--- pages/trainer/[name]/catch.vue
+++ pages/trainer/[name]/catch.vue
@@ -80,16 +80,13 @@ export default {
       <p v-else text-align: center>{{ page + 1 }} / {{ maxPage + 1 }} ページ</p>
     </GamifyList>
     <GamifyList>
-      <GamifyItem
-        v-for="(pokemon, index) in pokemons.results"
-        :key="pokemon.url"
-      >
+      <GamifyItem v-for="pokemon in pokemons.results" :key="pokemon.url">
         <!-- <img :src="pokemon.sprites.front_default" /> -->
         <!-- <img
           src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
         /> -->
         <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{{page}}.png"
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${page}.png"
         />
         <!-- <img :src="pokemon.sprites.front_default" /> -->
         <span class="pokemon-name">{{ pokemon.name }}</span>
